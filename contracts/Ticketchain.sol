6a2a472151b1dea285232a76ebd2a2d47e1ee5ff
Mustafa Osman <79769028+mustythefirst@users.noreply.github.com>
2023-05-03 21:58:00 +0100

    Update Ticketchain.sol

--- contracts/Ticketchain.sol
+++ contracts/Ticketchain.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Ticketchain is ERC1155, Ownable {
     uint256 public totalSupply;
 
-    constructor() ERC1155("https://rainbowkit-mint-nft-demo.vercel.app/nft.json") {}
+    constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmSNBEA9HHyYJFwHok9s6p71APdJxMynEDWTDfAJ3Lvpj6?_gl=1*wdad26*rs_ga*MGQwZDE3NGEtMmQyYi00MmNkLWIzMDUtZjkwMDg2YjhlYmVh*rs_ga_5RMPXG14TE*MTY4MzE0Njk0OC4yLjEuMTY4MzE0NzI0MS41NS4wLjA.") {}
 
     function mint() public {
         unchecked {
